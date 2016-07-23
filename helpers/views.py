34365a0156d33f78e7e8a391eb8074059c03911d
Anton Derugin <webstores@mail.ru>
2016-07-23 22:03:33 +0500

    [fix]

--- helpers/views.py
+++ helpers/views.py
@@ -138,7 +138,7 @@ class AjaxFormViewMixin(object):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context[self.context_form_name] = self.form
+        context[self.context_form_name] = self.form()
         return context
 
 
